54349d8f3c8c726c234de2cc2b8a9f7af8955767
neroanthony <50128636+neroanthony@users.noreply.github.com>
2019-07-17 00:37:29 +0700

    a

--- js/controller.js
+++ js/controller.js
@@ -5,15 +5,15 @@ const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\
 controller.validateLoginInfo = (email, password) => {
 
     if (!email) {        
-        view.renderErrorMessage("email-error-message","Please enter email");
+        view.renderErrorMessage("email-error-message","Enter your Email Address");
     } else if (!emailRegex.test(email)) {
-        view.renderErrorMessage("email-error-message","Invalid email address");
+        view.renderErrorMessage("email-error-message","Invalid Email Address");
     } else {
         view.renderErrorMessage("email-error-message","");
     }
 
     if (!password){
-        view.renderErrorMessage("password-error-message","Please enter password");
+        view.renderErrorMessage("password-error-message","Enter your Password");
     } else {
         view.renderErrorMessage("password-error-message","");
     }
@@ -27,27 +27,27 @@ controller.validateLoginInfo = (email, password) => {
 controller.validateRegisterInfo = (firstName, lastName, email, password, confirmPassword) => {
 
     if (!lastName || !firstName) {
-        view.renderErrorMessage("name-error-message","Please enter your First Name and Last Name");
+        view.renderErrorMessage("name-error-message","Enter your First Name and Last Name");
     } else {
         view.renderErrorMessage("name-error-message","");
     }
     
     if (!email) {        
-        view.renderErrorMessage("email-error-message","Please enter email");
+        view.renderErrorMessage("email-error-message","Enter your Email Address");
     } else if (!emailRegex.test(email)) {
-        view.renderErrorMessage("email-error-message","Invalid email address");
+        view.renderErrorMessage("email-error-message","Invalid Email Address");
     } else {
         view.renderErrorMessage("email-error-message","");
     }
 
     if (!password){
-        view.renderErrorMessage("password-error-message","Please enter password");
+        view.renderErrorMessage("password-error-message","Enter your Password");
     } else {
         view.renderErrorMessage("password-error-message","");
     }
 
     if(!confirmPassword) {
-        view.renderErrorMessage("confirm-password-error-message","Please enter password");
+        view.renderErrorMessage("confirm-password-error-message","Enter your Confirm Password");
     } else if(password != confirmPassword){
         view.renderErrorMessage("confirm-password-error-message","Two passwords do not match");
     } else{
